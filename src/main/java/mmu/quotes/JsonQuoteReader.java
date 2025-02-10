9bab8c394ab5dd416d3165ec01381be947ec8ac1
JACaddock <jamesacaddock@gmail.com>
2025-02-10 14:09:23 +0000

    Fixed JsonQuoteReader and added quotes.json

--- src/main/java/mmu/quotes/JsonQuoteReader.java
+++ src/main/java/mmu/quotes/JsonQuoteReader.java
@@ -3,6 +3,7 @@ package mmu.quotes;
 import java.util.*;
 import java.io.*;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class JsonQuoteReader implements QuoteReader {
     private String filePath;
@@ -11,8 +12,7 @@ public class JsonQuoteReader implements QuoteReader {
         this.filePath = filePath;
     }
     
-    public List<Map<String, Object>> readQuotes() throws FileNotFoundException
-    {
+    public List<Map<String, Object>> readQuotes() throws FileNotFoundException {
         Gson gson = new Gson();
         FileReader fileReader = new FileReader(filePath);
         return gson.fromJson(fileReader,
